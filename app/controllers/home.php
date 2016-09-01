c7f143ee583d87b196d1ddcfd79b81479bfefe86
milosd92 <milosdanilov@gmail.com>
2016-09-01 21:32:07 +0200

    Adding a basic view functionality

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -6,6 +6,7 @@ class Home extends Controller
     {
         $user = $this->model('User');
         $user->name = $name;
-        echo $user->name;
+
+        $this->view('home/index', ['name' => $user->name]);
     }
 }
